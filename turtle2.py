16ed3d3cc8fad23b488bc22f433362423a63d838
wprkfrnjs <112221356+wprkfrnjs@users.noreply.github.com>
2022-08-27 10:32:45 +0900

    Add files via upload

---
+++ turtle2.py
@@ -0,0 +1,18 @@
+import turtle
+t= turtle.Turtle()
+t.shape("turtle")
+
+a=200
+b=80
+for i in range(20):
+    t.forward(a)
+    t.left(b)
+
+
+
+#t.forward(100)
+#t.left(90)
+#t.forward(100)
+#t.left(90)
+#t.forward(100)
+#t.left(90)
